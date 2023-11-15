1cf8dfc0f7e79540802f6ef96019becfecb3eb4b
Burton Arceneaux <btarceneaux@gmail.com>
2023-11-15 12:31:59 -0800

    Initial commit.

---
+++ src/main/java/inc/foodie/FoodieIncOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package inc.foodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieIncOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieIncOrderServiceApplication.class, args);
+	}
+
+}
